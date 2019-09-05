4a535065b0d09e07c069c6bc548ad2e9b46d0ed7
Emily Schlicter <35039378+eschlicter@users.noreply.github.com>
2019-09-05 15:10:25 -0500

    updated routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,12 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 
 export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
     new Recipe('Fish Tacos', 
@@ -37,5 +37,9 @@ export class RecipeService{
     addIngredientToShoppingList(ingredients: Ingredient[]){
         this.slService.addIngredients(ingredients);
 
+    }
+
+    getRecipe(index: number){
+        return this.recipes[index];
     }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -11,14 +12,20 @@ import { ShoppingListService } from './shopping-list.service';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[])=> {
+    this.subscription = this.slService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[])=> {
       this.ingredients = ingredients;
-    })
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
  
 }
